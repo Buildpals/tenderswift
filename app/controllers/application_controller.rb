12b1ef88d24c37df6bc545d7d17754e380ee8b5f
bkwaku <kbboateng14@gmail.com>
2017-08-30 23:39:29 +0100

    Add sign out buttons and error message for confirmable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(quantity_surveyor)
+    request_for_tenders_path
+  end
+
+  def after_sign_up_path_for(quantity_surveyor)
+    request_for_tenders_path
+  end
+
+  def after_inactive_sign_up_path_for(quantity_surveyor)
+    new_quantity_surveyor_session_path
+  end
+
   protected
   
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-
+  devise_for :quantity_surveyors
+  
   resources :items
   resources :sections
   resources :pages
   resources :boqs
   resources :participants
   resources :request_for_tenders
-  devise_for :quantity_surveyors
   resources :filled_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
