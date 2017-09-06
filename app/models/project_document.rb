d46143e8df38cd37bdad071ebd6d7523bebd52e4
bkwaku <kbboateng14@gmail.com>
2017-09-06 15:31:55 +0100

    Forces QS to upload only word, auto card and pdf files

--- app/models/project_document.rb
+++ app/models/project_document.rb
@@ -2,4 +2,18 @@ class ProjectDocument < ApplicationRecord
   mount_uploader :document, DocumentUploader
 
   belongs_to :request_for_tender
+
+  validate :check_file_extension
+
+  private
+
+  def check_file_extension
+    if document
+      accepted_formats = ['.docx', '.pdf', '.dwg', '.dxf']
+      unless accepted_formats.include? File.extname(document.file.path)
+        errors.add(:document, :invalid, message: "The uploaded document should be a PDF, Word Document or Auto Card file.")
+      end
+    end
+  end
+  
 end
