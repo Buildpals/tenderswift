1620ac2ab8b07cb81cc7d0ee14449e6b5ee9f8f7
bkwaku <kbboateng14@gmail.com>
2017-09-11 10:06:18 +0100

    Style footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -106,23 +106,3 @@ function saveFilledItem(filledItem) {
         });
 }
 
-
-data = {
-  datasets: [{
-      data: [10, 20, 30]
-  }],
-
-  // These labels appear in the legend and in the tooltips when hovering different arcs
-  labels: [
-      'Red',
-      'Yellow',
-      'Blue'
-  ]
-};
-
-// And for a doughnut chart
-var myDoughnutChart = new Chart(ctx, {
-  type: 'doughnut',
-  data: data,
-  options: options
-});
