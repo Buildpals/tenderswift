efe9151bf385ca0df7f48387620866526326f8a2
bkwaku <kbboateng14@gmail.com>
2017-09-11 14:36:24 +0100

    Add fontEnd validations

--- app/models/quantity_surveyor.rb
+++ app/models/quantity_surveyor.rb
@@ -6,7 +6,16 @@ class QuantitySurveyor < ApplicationRecord
 
   has_many :request_for_tenders
 
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  
+  EMAIL_MESSAGE = "Please provide an correct email address with more than 5 characters"
+
   validates :company_name, presence: true
+
+  validates :email, presence: { message: EMAIL_MESSAGE }, uniqueness: { message: EMAIL_MESSAGE, case_sensitive: false }, length: { within: 5...100 }, format: EMAIL_REGEX
+
+  validates :phone_number, presence: true, uniqueness: true
+   
 
   def name
     "#{company_name} (#{email})"
