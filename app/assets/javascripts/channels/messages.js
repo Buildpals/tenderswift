96e2a182f57fbfecb1cfc71036f0728916f34842
bkwaku <kbboateng14@gmail.com>
2017-09-16 16:44:00 +0100

    Make broadcasts appear only to participants
    Broadcast use to appear to participants who were not included in the tender

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -8,24 +8,26 @@ App.cable = ActionCable.createConsumer();
 
 App.messages = App.cable.subscriptions.create('MessagesChannel', {  
   received: function(data) {
-    //console.log(data);
-    //check if the participant is part of the participants of the request for tender
-    return $('.new-broadcast-messages').append(
-      '<br/>'+
-      '<div class="card">'+
-      '   <div class="card-body">'+
-      '    <h4 class="card-title"><span class="fa fa-bullhorn"></span></h4>'+
-      '    <h6 class="card-subtitle mb-2 text-muted">All Participants</h6>'+
-      '    <p class="card-text">'+data.message.content+'</p>'+
-      '    <small class="float-right">'+
-      '          <span class="fa fa-time"></span>'+data.formatted_date+
-      '    </small>'+
-      '   </div>'+
-      '</div>'
-    );
-  },
-
-  renderMessage: function(data) {
-    return true;
-  }
+    console.log(data);
+    var current_request_id = parseInt($(".hidden-request-id").text());
+    console.log(current_request_id);
+    console.log(data.request_for_tender_id);
+    if(current_request_id === parseInt(data.request_for_tender_id)){
+          //check if the participant is part of the participants of the request for tender
+        return $('.new-broadcast-messages').append(
+          '<br/>'+
+          '<div class="card">'+
+          '   <div class="card-body">'+
+          '    <h4 class="card-title"><span class="fa fa-bullhorn"></span></h4>'+
+          '    <h6 class="card-subtitle mb-2 text-muted">All Participants</h6>'+
+          '    <p class="card-text">'+data.message.content+'</p>'+
+          '    <small class="float-right">'+
+          '          <span class="fa fa-time"></span>'+data.formatted_date+
+          '    </small>'+
+          '   </div>'+
+          '</div>'
+        );
+      }
+      else{ return true }
+    }
 });
--- app/models/broadcast_message.rb
+++ app/models/broadcast_message.rb
@@ -5,7 +5,7 @@ class BroadcastMessage < ApplicationRecord
 
   has_one :quantity_surveyor
   
-  after_create :send_data_to_action_cable
+  after_create :send_data_to_message_channel
 
 
 
@@ -15,10 +15,13 @@ class BroadcastMessage < ApplicationRecord
     self.created_at.to_formatted_s(:long)
   end
 
-  def send_data_to_action_cable
+  def send_data_to_message_channel
     ActionCable.server.broadcast 'messages',
     message: self,
     formatted_date: format_day_created,
+    request_for_tender_id: self.chatroom.request_for_tender.id,
     qs: self.chatroom.request_for_tender.quantity_surveyor
   end
 end
+
+#if the request_for_tender_id of the broadcast is equal to the request_for_tender_id of the participant
