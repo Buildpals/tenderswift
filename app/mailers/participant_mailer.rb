c2a6aa985ac0c51b708ca6e03a531f589b44b090
bkwaku <kbboateng14@gmail.com>
2017-09-25 12:27:51 +0100

    Add consultant name to email for invite to tender

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -1,8 +1,9 @@
 class ParticipantMailer < ApplicationMailer
   default from: 'kwaku@buildpals.com'
 
-  def request_for_tender_email(participant)
+  def request_for_tender_email(participant, request_for_tender)
     @participant = participant
+    @request_for_tender = request_for_tender
     mail(to: @participant.email,
          subject: "Invitation to Tender for #{@participant.project_name}"
     )
