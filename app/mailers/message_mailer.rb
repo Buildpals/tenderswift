772149274908638cf06a7d101a0c2e6d8a4cffda
bkwaku <kbboateng14@gmail.com>
2017-09-25 12:54:51 +0100

    Change email for messaging

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -5,7 +5,7 @@ class MessageMailer < ApplicationMailer
   def deliver_message_email(message)
     @message = message
     mail(to: @message.participant.email,
-        subject: "You have a new messge from #{@message.broadcast_message.chatroom.request_for_tender.project_name}"
+        subject: "You have a new messge from #{@message.broadcast_message.chatroom.request_for_tender.quantity_surveyor.company_name}"
     )
   end
 end
