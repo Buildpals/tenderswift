0771da2e3ae12e886d2ba8c3659b8489cb8590e9
bkwaku <kbboateng14@gmail.com>
2017-09-25 12:48:57 +0100

    Remove fit to N+1 problem

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -7,7 +7,7 @@ class ParticipantsController < ApplicationController
   # GET /participants
   # GET /participants.json
   def index
-    @participants = Participant.all.includes(:messages, :answers, :items, :filled_items)
+    @participants = Participant.all
   end
 
   # GET /participants/1
@@ -92,7 +92,7 @@ class ParticipantsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_participant
-    @participant = Participant.includes(:messages, :answers, :items, :filled_items).find_by(auth_token: params[:id])
+    @participant = Participant.find_by(auth_token: params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
