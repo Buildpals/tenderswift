1618ec8dbfc04a008dc34d0288c04ab13421d4f8
Peter O'Hara Adu <ohara.invent@gmail.com>
2017-12-02 16:33:29 +0000

    Remove tags and sections table from the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171202162513) do
+ActiveRecord::Schema.define(version: 20171202162911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,21 @@ ActiveRecord::Schema.define(version: 20171202162513) do
     t.index ["request_for_tender_id"], name: "index_boqs_on_request_for_tender_id"
   end
 
+  create_table "broadcast_messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "chatroom_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_broadcast_messages_on_chatroom_id"
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.bigint "request_for_tender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["request_for_tender_id"], name: "index_chatrooms_on_request_for_tender_id"
+  end
+
   create_table "countries", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -109,6 +124,17 @@ ActiveRecord::Schema.define(version: 20171202162513) do
     t.index ["boq_id"], name: "index_items_on_boq_id"
     t.index ["page_id"], name: "index_items_on_page_id"
     t.index ["section_id"], name: "index_items_on_section_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "broadcast_message_id"
+    t.bigint "participant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "sender", default: 0
+    t.index ["broadcast_message_id"], name: "index_messages_on_broadcast_message_id"
+    t.index ["participant_id"], name: "index_messages_on_participant_id"
   end
 
   create_table "pages", force: :cascade do |t|
@@ -212,22 +238,6 @@ ActiveRecord::Schema.define(version: 20171202162513) do
     t.index ["quantity_surveyor_id"], name: "index_request_for_tenders_on_quantity_surveyor_id"
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.string "name"
-    t.bigint "page_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["page_id"], name: "index_sections_on_page_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.bigint "boq_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["boq_id"], name: "index_tags_on_boq_id"
-  end
-
   create_table "winners", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
@@ -244,11 +254,14 @@ ActiveRecord::Schema.define(version: 20171202162513) do
   add_foreign_key "answer_boxes", "participants"
   add_foreign_key "answer_boxes", "questions"
   add_foreign_key "answer_documents", "answer_boxes"
+  add_foreign_key "broadcast_messages", "chatrooms"
+  add_foreign_key "chatrooms", "request_for_tenders"
   add_foreign_key "filled_items", "items"
   add_foreign_key "filled_items", "participants"
   add_foreign_key "items", "pages"
+  add_foreign_key "messages", "broadcast_messages"
+  add_foreign_key "messages", "participants"
   add_foreign_key "project_documents", "request_for_tenders"
   add_foreign_key "questions", "request_for_tenders"
-  add_foreign_key "tags", "boqs"
   add_foreign_key "winners", "request_for_tenders"
 end
