4214f297400d714f5fa24b549c15c1ade670822c
bkwaku <kbboateng14@gmail.com>
2018-01-21 23:08:08 +0000

    Fix contract sum bug of not adding up well

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -80,6 +80,6 @@ class RatesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rate_params
-      params.require(:rate).permit(:boq_id, :sheet_name, :row_number, :value, :participant_id)
+      params.require(:rate).permit(:boq_id, :sheet_name, :row_number, :value, :participant_id, :quantity)
     end
 end
