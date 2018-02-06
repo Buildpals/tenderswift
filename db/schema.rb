e28c5313eca5d2bab69adc378ab93ed2f4e4ba4f
bkwaku <kbboateng14@gmail.com>
2018-02-06 14:48:12 +0000

    Add account number to Request For Tender

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206144450) do
+ActiveRecord::Schema.define(version: 20180206144613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -279,6 +279,7 @@ ActiveRecord::Schema.define(version: 20180206144450) do
     t.string "bank_name"
     t.string "branch_name"
     t.string "account_name"
+    t.string "account_number"
     t.index ["country_id"], name: "index_request_for_tenders_on_country_id"
     t.index ["quantity_surveyor_id"], name: "index_request_for_tenders_on_quantity_surveyor_id"
   end
