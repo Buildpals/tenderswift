681d50c76d1f60b7feb6cbaab6f525630fd556f5
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-03-10 18:57:55 +0000

    Add and configure pundit gem for authorisation

--- Gemfile
+++ Gemfile
@@ -80,6 +80,9 @@ gem 'redis', '~> 3.0'
 # WYSIWYG Editor
 gem 'trix'
 
+# Authorisation
+gem 'pundit'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
   def after_sign_in_path_for(user)
     if user.class.eql?(QuantitySurveyor)
@@ -20,8 +25,21 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name phone_number company_name company_logo])
+  end
+
+  def pundit_user
+    current_quantity_surveyor
+  end
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 end
