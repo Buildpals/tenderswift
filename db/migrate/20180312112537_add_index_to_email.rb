eefc3d6e74e74c87c0a41c25dc4fdaf774902a8d
bkwaku <kbboateng14@gmail.com>
2018-03-12 11:28:52 +0000

    Tried to post an index on quantity ssurveyor email
    Guess what? we already had one

---
+++ db/migrate/20180312112537_add_index_to_email.rb
@@ -0,0 +1,4 @@
+class AddIndexToEmail < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
---
+++ db/migrate/20180312112603_add_index_quantity_surveyor_email.rb
@@ -0,0 +1,4 @@
+class AddIndexQuantitySurveyorEmail < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
