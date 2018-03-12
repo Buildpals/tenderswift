e476daf5fce4f89e2bd1428a7362f1098321f1bb
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-03-12 10:56:29 +0000

    Temporarily remove verify_authorization in every action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  after_action :verify_authorized, except: :index, unless: :devise_controller?
-  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+  # after_action :verify_authorized, except: :index, unless: :devise_controller?
+  # after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
   def after_sign_in_path_for(user)
     if user.class.eql?(QuantitySurveyor)
