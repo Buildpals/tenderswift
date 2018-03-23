82b00cc1c342471d69c71e72142e75e06df03de4
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-03-23 22:21:50 +0000

    Remove "Get #new" spec for request_for_tenders_controller
    Remove "Get #new" spec for request_for_tenders_controller cos
    the new action for request_for_tenders controller  is not used
    and has been deleted

--- spec/controllers/request_for_tenders_controller_spec.rb
+++ spec/controllers/request_for_tenders_controller_spec.rb
@@ -57,13 +57,6 @@ RSpec.describe RequestForTendersController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns a success response" do
-      get :new, params: {}, session: valid_session
-      expect(response).to be_success
-    end
-  end
-
   describe "GET #edit" do
     it "returns a success response" do
       request_for_tender = RequestForTender.create! valid_attributes
