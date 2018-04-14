f885e2a76aa9e28e1e5fb655e98539e60d385f81
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-04-14 07:33:42 +0000

    Remove unused methods from application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,70 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
-  def page_as_json(page, tender = nil)
-    sheet_data = []
-    page.sections.each do |section|
-      create_section_header(section, sheet_data)
-      if tender
-        add_items_and_filled_items_to_section(section, sheet_data, tender)
-      else
-        add_items_to_section(section, sheet_data)
-      end
-    end
-    raw sheet_data.to_json
-  end
-
-  private
-
-  def create_section_header(section, sheet_data)
-    sheet_data.push(description: section.name)
-  end
-
-  def add_items_to_section(section, sheet_data)
-    section.items.each do |item|
-      sheet_data.push(item: item.name,
-                      description: item.description,
-                      quantity: item.quantity,
-                      unit: item.unit)
-    end
-  end
-
-  def add_items_and_filled_items_to_section(section, sheet_data, tender)
-    section.items.each do |item|
-      sheet_data.push(item: item.name,
-                      description: item.description,
-                      quantity: item.quantity,
-                      unit: item.unit,
-                      filled_item:  tender.filled_item(item))
-    end
-  end
-
-
-  def is_admin_signin?
-    request_path = request.fullpath.split('/')
-    if request_path[1].eql?('admins')
-      return true
-    else
-      return false
-    end
-  end
-
-  def get_index_of_alphabet(alphabet)
-    alphabets = ('A'...'z').to_a
-    if alphabets.include?(alphabet)
-        return alphabets.index(alphabet)
-    end
-  end
-
-  def pdf?(path)
-    true if "pdf".include? File.extname(path)
-  end
-
-
   require 'net/http'
 
   def working_url?(url_str)
     return false if url_str.nil?
     uri = URI.parse(URI.encode(url_str))
-    if uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS)
+    if uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
       true
     else
       false
@@ -78,7 +20,7 @@ module ApplicationHelper
   end
 
   def get_month(index)
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    months = %w[January February March April May June July August September October November December]
     months[index]
   end
 
