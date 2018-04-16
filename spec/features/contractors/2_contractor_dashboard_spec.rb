8b4f4694b97eee2a9aa80a4ef4dfd065af2c4813
Bkwaku <kwaku.ahonya@gmail.com>
2018-04-16 13:02:04 +0000

    Tests to see if contractor can see purcahsed tender documents

--- spec/features/contractors/2_contractor_dashboard_spec.rb
+++ spec/features/contractors/2_contractor_dashboard_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.feature 'Contractor dashboard' do
   let!(:tender) { FactoryBot.create(:tender) }
-
+  let(:purchased_tender) { FactoryBot.create(:tender) } 
   context 'when logged in' do
     background do
       login_as(tender.contractor, scope: :contractor)
@@ -17,12 +17,12 @@ RSpec.feature 'Contractor dashboard' do
 
     scenario 'A contractor can view their private invitations to tender on their dashboard' do
       visit contractor_root_path
-      # expect(tender.contractor.tenders).to be > 0
       expect(page).to have_content tender.project_name
     end
 
     scenario 'A contractor can view their purchased tender documents on their dashboard' do
-      skip 'Spec not finished'
+      visit contractor_root_path
+      expect(page).to have_content purchased_tender.project_name
     end
 
     scenario 'A contractor can view their submitted tender documents on their dashboard' do
