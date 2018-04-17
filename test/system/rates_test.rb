f0f97d1bfcb00a38178380eda5f969cdb848df39
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-04-17 10:18:33 +0000

    Delete unused minitest tests

--- test/system/rates_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class RatesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit rates_url
-  #
-  #   assert_selector "h1", text: "Rate"
-  # end
-end
--- test/system/request_for_tenders_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class RequestForTendersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit request_for_tenders_url
-  #
-  #   assert_selector "h1", text: "RequestForTender"
-  # end
-end
