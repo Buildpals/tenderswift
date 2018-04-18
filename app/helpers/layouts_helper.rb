bbfd5ae3e0d2cc40def7e4d7a9e21243cd199e3f
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-04-18 19:17:29 +0000

    Delete unnecessary file layouts_helper.rb

--- app/helpers/layouts_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module LayoutsHelper
-  def parent_layout(layout)
-    @view_flow.set(:layout, output_buffer)
-    output = render(file: "layouts/#{layout}")
-    self.output_buffer = ActionView::OutputBuffer.new(output)
-  end
-end
