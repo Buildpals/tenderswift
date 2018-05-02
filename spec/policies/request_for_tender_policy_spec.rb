c22280effae35b0888b5cd9882a309bf693c3322
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-05-02 18:54:59 +0000

    Fix spec for scope in request_for_tender_policy

--- spec/policies/request_for_tender_policy_spec.rb
+++ spec/policies/request_for_tender_policy_spec.rb
@@ -6,17 +6,19 @@ RSpec.describe RequestForTenderPolicy do
   subject { described_class.new(quantity_surveyor, request_for_tender) }
 
   let(:resolved_scope) do
-    described_class::Scope.new(quantity_surveyor, RequestForTender).resolve
+    described_class::Scope.new(quantity_surveyor, RequestForTender.all).resolve
   end
 
   context 'quantity_surveyor owns the request_for_tender' do
-    let(:quantity_surveyor) { QuantitySurveyor.new(id: 1) }
-    let(:request_for_tender) { RequestForTender.create(quantity_surveyor_id: 1) }
+    let(:quantity_surveyor) { FactoryBot.create(:quantity_surveyor) }
+    let(:request_for_tender) do
+      FactoryBot.create(:request_for_tender,
+                        quantity_surveyor: quantity_surveyor)
+    end
 
-    # it 'includes request_for_tender in resolved scope' do
-    #   a = resolved_scope
-    #   expect(a).to include(request_for_tender)
-    # end
+    it 'includes request_for_tender in resolved scope' do
+      expect(resolved_scope).to include(request_for_tender)
+    end
 
     it { is_expected.to permit_action(:edit_tender_information) }
     it { is_expected.to permit_action(:update_tender_information) }
@@ -34,6 +36,7 @@ RSpec.describe RequestForTenderPolicy do
     it { is_expected.to permit_action(:update_tender_contractors) }
 
     it { is_expected.to permit_action(:create) }
+
     it { is_expected.to permit_action(:show) }
     it { is_expected.to permit_action(:compare_boq) }
     it { is_expected.to permit_action(:update) }
@@ -41,32 +44,36 @@ RSpec.describe RequestForTenderPolicy do
   end
 
   context 'quantity_surveyor does not own the request_for_tender' do
-    let(:quantity_surveyor) { QuantitySurveyor.new(id: 1) }
-    let(:request_for_tender) { RequestForTender.create(quantity_surveyor_id: 2) }
+    let(:quantity_surveyor) { FactoryBot.create(:quantity_surveyor) }
+    let(:request_for_tender) do
+      FactoryBot.create(:request_for_tender)
+    end
 
-    # it 'includes request_for_tender in resolved scope' do
-    #   expect(resolved_scope).not_to include(request_for_tender)
-    # end
 
-    it { is_expected.not_to permit_action(:edit_tender_information) }
-    it { is_expected.not_to permit_action(:update_tender_information) }
-    it { is_expected.not_to permit_action(:edit_tender_boq) }
-    it { is_expected.not_to permit_action(:update_tender_boq) }
-    it { is_expected.not_to permit_action(:update_contract_sum_address) }
-    it { is_expected.not_to permit_action(:edit_tender_documents) }
-    it { is_expected.not_to permit_action(:update_tender_documents) }
-    it { is_expected.not_to permit_action(:edit_tender_required_documents) }
-    it { is_expected.not_to permit_action(:update_tender_required_documents) }
-    it { is_expected.not_to permit_action(:edit_tender_payment_method) }
-    it { is_expected.not_to permit_action(:update_tender_payment_method) }
-    it { is_expected.not_to permit_action(:update_payment_details) }
-    it { is_expected.not_to permit_action(:edit_tender_contractors) }
-    it { is_expected.not_to permit_action(:update_tender_contractors) }
+    it 'excludes request_for_tender from resolved scope' do
+      expect(resolved_scope).not_to include(request_for_tender)
+    end
+
+    it { is_expected.to forbid_action(:edit_tender_information) }
+    it { is_expected.to forbid_action(:update_tender_information) }
+    it { is_expected.to forbid_action(:edit_tender_boq) }
+    it { is_expected.to forbid_action(:update_tender_boq) }
+    it { is_expected.to forbid_action(:update_contract_sum_address) }
+    it { is_expected.to forbid_action(:edit_tender_documents) }
+    it { is_expected.to forbid_action(:update_tender_documents) }
+    it { is_expected.to forbid_action(:edit_tender_required_documents) }
+    it { is_expected.to forbid_action(:update_tender_required_documents) }
+    it { is_expected.to forbid_action(:edit_tender_payment_method) }
+    it { is_expected.to forbid_action(:update_tender_payment_method) }
+    it { is_expected.to forbid_action(:update_payment_details) }
+    it { is_expected.to forbid_action(:edit_tender_contractors) }
+    it { is_expected.to forbid_action(:update_tender_contractors) }
 
     it { is_expected.to permit_action(:create) }
-    it { is_expected.not_to permit_action(:show) }
-    it { is_expected.not_to permit_action(:compare_boq) }
-    it { is_expected.not_to permit_action(:update) }
-    it { is_expected.not_to permit_action(:destroy) }
+
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_action(:compare_boq) }
+    it { is_expected.to forbid_action(:update) }
+    it { is_expected.to forbid_action(:destroy) }
   end
 end
