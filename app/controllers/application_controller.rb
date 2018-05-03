e694ba7266d1a3108172b17e6573b8478c2d7fe7
Kwaku <kwaku.ahonya@gmail.com>
2018-05-03 11:25:34 +0000

    Add spec for contractor policy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  after_action :verify_authorized, except: :index, unless: :devise_controller?
-  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
+  after_action :verify_authorized,
+               except: :index,
+               unless: :devise_controller?
+
+  after_action :verify_policy_scoped,
+               only: :index,
+               unless: -> { return devise_controller? || welcome_controller? }
 
   protected
 
@@ -17,5 +22,9 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = 'You are not authorized to perform this action.'
     redirect_to(request.referrer || root_path)
+  end
+
+  def welcome_controller?
+    controller_name == 'welcome'
   end
 end
--- spec/policies/contractor_policy_spec.rb
+++ spec/policies/contractor_policy_spec.rb
@@ -3,4 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe ContractorPolicy do
+  subject { described_class.new(contractor, contractor_profile) }
+
+  context 'when a contractor owns a contractor profile' do
+    let(:contractor) { FactoryBot.create(:contractor) }
+    let(:contractor_profile) { contractor }
+
+    it { is_expected.to permit_action(:dashboard) }
+    it { is_expected.to permit_action(:edit) }
+    it { is_expected.to permit_action(:update) }
+
+  end
 end
