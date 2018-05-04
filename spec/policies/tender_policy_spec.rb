cd8e72728accddc3c0bd519aa4cc6277b2134c5c
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-05-04 16:45:21 +0100

    Make sure quantity surveyor can access the corresponding bids

--- spec/policies/tender_policy_spec.rb
+++ spec/policies/tender_policy_spec.rb
@@ -27,4 +27,34 @@ RSpec.describe TenderPolicy do
     it { is_expected.to forbid_action(:save_rates) }
     it { is_expected.to forbid_action(:save_contractors_documents) }
   end
+
+  subject { described_class.new(quantity_surveyor, tender) }
+
+  let(:quantity_surveyor) { FactoryBot.build(:quantity_surveyor) }
+  let(:contractor) { FactoryBot.build(:contractor) }
+
+  context 'quantity surveyor owns a request for tender' do
+    let(:request_for_tender) { FactoryBot.build(:request_for_tender,
+                                                quantity_surveyor: quantity_surveyor) }
+    let(:tender) { FactoryBot.build(:purchased_tender, contractor: contractor,
+                                    request_for_tender: request_for_tender) }
+
+    it { is_expected.to permit_action(:other_documents) }
+    it { is_expected.to permit_action(:disqualify) }
+    it { is_expected.to permit_action(:undo_disqualify) }
+    it { is_expected.to permit_action(:rate) }
+    it { is_expected.to permit_action(:required_documents) }
+  end
+
+  context 'quantity surveyor does not owns a request for tender' do
+    let(:request_for_tender) { FactoryBot.build(:request_for_tender) }
+    let(:tender) { FactoryBot.build(:purchased_tender, contractor: contractor,
+                                    request_for_tender: request_for_tender) }
+    it { is_expected.to forbid_action(:other_documents) }
+    it { is_expected.to forbid_action(:disqualify) }
+    it { is_expected.to forbid_action(:undo_disqualify) }
+    it { is_expected.to forbid_action(:rate) }
+    it { is_expected.to forbid_action(:required_documents) }
+  end
+
 end
