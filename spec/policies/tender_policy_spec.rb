5661c1009216803440e1d436abea0c242c60f613
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-05-04 16:23:49 +0100

    Check if contractor can access tender without buying it

--- spec/policies/tender_policy_spec.rb
+++ spec/policies/tender_policy_spec.rb
@@ -8,14 +8,23 @@ RSpec.describe TenderPolicy do
   let(:quantity_surveyor) { FactoryBot.build(:quantity_surveyor) }
   let(:request_for_tender) { FactoryBot.build(:request_for_tender) }
   let(:contractor) { FactoryBot.build(:contractor) }
-  let(:tender) { FactoryBot.build(:purchased_tender, contractor: contractor) }
 
-  context 'contractor owns a tender' do
+  context 'contractor owns a purchased tender' do
+    let(:tender) { FactoryBot.build(:purchased_tender, contractor: contractor) }
     it { is_expected.to permit_action(:project_information) }
     it { is_expected.to permit_action(:boq) }
     it { is_expected.to permit_action(:contractors_documents) }
     it { is_expected.to permit_action(:results) }
     it { is_expected.to permit_action(:save_rates) }
     it { is_expected.to permit_action(:save_contractors_documents) }
+  end
+
+  context 'contractor does not own a purchased tender' do
+    let(:tender) { FactoryBot.build(:tender, contractor: contractor) }
+    it { is_expected.to forbid_action(:boq) }
+    it { is_expected.to forbid_action(:contractors_documents) }
+    it { is_expected.to forbid_action(:results) }
+    it { is_expected.to forbid_action(:save_rates) }
+    it { is_expected.to forbid_action(:save_contractors_documents) }
   end
 end
