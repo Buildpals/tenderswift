f081f8a45001fe66c0a88e919394ff84da870f66
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-05-07 22:38:07 +0000

    Fix required_document_upload_policy and it's spec

--- spec/policies/required_document_upload_policy_spec.rb
+++ spec/policies/required_document_upload_policy_spec.rb
@@ -5,49 +5,101 @@ require 'rails_helper'
 RSpec.describe RequiredDocumentUploadPolicy do
   subject { described_class.new(quantity_surveyor, required_document_upload) }
 
-  context 'quantity surveyor owns the request for tender for the ' \
-          'required document upload' do
-    let(:quantity_surveyor) { FactoryBot.create(:quantity_surveyor) }
-    let(:request_for_tender) { FactoryBot.create(:request_for_tender,
-                                                 quantity_surveyor: quantity_surveyor) }
-    let(:required_document) { FactoryBot.create(:required_document,
-                                                request_for_tender:
-                                                    request_for_tender)}
-    let(:tender) { FactoryBot.create(:tender,
-                                     request_for_tender: request_for_tender) }
-    let(:required_document_upload) { FactoryBot.create(:required_document_upload,
-                                                        tender: tender,
-                                                        required_document:
-                                                            required_document) }
+  let(:quantity_surveyor) { FactoryBot.create(:quantity_surveyor) }
 
+  context 'quantity_surveyor does not own the required_document\s ' \
+          'request_for_tender, the tender is not reviewable' do
 
-    it { is_expected.to permit_action(:pdf_viewer) }
-    it { is_expected.to permit_action(:image_viewer) }
-    it { is_expected.to permit_action(:update) }
+    let(:request_for_tender) do
+      FactoryBot.create(:request_for_tender)
+    end
 
+    let(:tender) do
+      FactoryBot.create(:tender,
+                        request_for_tender: request_for_tender)
+    end
 
+    let(:required_document_upload) do
+      FactoryBot.create(:required_document_upload,
+                        tender: tender)
+    end
+
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_action(:approve) }
+    it { is_expected.to forbid_action(:reject) }
   end
 
+  context 'quantity_surveyor does not own the required_document\s ' \
+          'request_for_tender, the tender is reviewable' do
 
-  context 'quantity surveyor does not own the request for tender for the ' \
-          'required document upload' do
-    let(:quantity_surveyor) { FactoryBot.create(:quantity_surveyor) }
-    let(:request_for_tender) { FactoryBot.create(:request_for_tender) }
-    let(:required_document) { FactoryBot.create(:required_document,
-                                                request_for_tender:
-                                                    request_for_tender)}
-    let(:tender) { FactoryBot.create(:tender,
-                                     request_for_tender: request_for_tender) }
-    let(:required_document_upload) { FactoryBot.create(:required_document_upload,
-                                                       tender: tender,
-                                                       required_document:
-                                                           required_document) }
+    let(:request_for_tender) do
+      FactoryBot.create(:request_for_tender,
+                        deadline: Time.current - 1.hours)
+    end
 
+    let(:tender) do
+      FactoryBot.create(:tender,
+                        request_for_tender: request_for_tender,
+                        purchased_at: Time.current - 2.days,
+                        submitted_at: Time.current - 1.days)
+    end
 
-    it { is_expected.to forbid_action(:pdf_viewer) }
-    it { is_expected.to forbid_action(:image_viewer) }
-    it { is_expected.to forbid_action(:update) }
+    let(:required_document_upload) do
+      FactoryBot.create(:required_document_upload,
+                        tender: tender)
+    end
 
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_action(:approve) }
+    it { is_expected.to forbid_action(:reject) }
+  end
 
+  context 'quantity_surveyor owns the required_document\s request_for_tender,' \
+          ' the tender is not reviewable' do
+
+    let(:request_for_tender) do
+      FactoryBot.create(:request_for_tender,
+                        quantity_surveyor: quantity_surveyor)
+    end
+
+    let(:tender) do
+      FactoryBot.create(:tender,
+                        request_for_tender: request_for_tender)
+    end
+
+    let(:required_document_upload) do
+      FactoryBot.create(:required_document_upload,
+                        tender: tender)
+    end
+
+    it { is_expected.to forbid_action(:show) }
+    it { is_expected.to forbid_action(:approve) }
+    it { is_expected.to forbid_action(:reject) }
+  end
+
+  context 'quantity_surveyor owns the required_document\s request_for_tender,' \
+          ' the tender is reviewable' do
+
+    let(:request_for_tender) do
+      FactoryBot.create(:request_for_tender,
+                        quantity_surveyor: quantity_surveyor,
+                        deadline: Time.current - 1.hours)
+    end
+
+    let(:tender) do
+      FactoryBot.create(:tender,
+                        request_for_tender: request_for_tender,
+                        purchased_at: Time.current - 2.days,
+                        submitted_at: Time.current - 1.days)
+    end
+
+    let(:required_document_upload) do
+      FactoryBot.create(:required_document_upload,
+                        tender: tender)
+    end
+
+    it { is_expected.to permit_action(:show) }
+    it { is_expected.to permit_action(:approve) }
+    it { is_expected.to permit_action(:reject) }
   end
 end
