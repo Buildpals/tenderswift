c63bf5f6bcb341b55af4c656f4b5bd4c1f23e4af
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-05-08 09:28:31 +0000

    Refactor bid policy for clarity

--- app/policies/bid_policy.rb
+++ app/policies/bid_policy.rb
@@ -9,35 +9,38 @@ class BidPolicy
   end
   
   def required_documents?
-    quantity_surveyor_created_this_tenders_request_for_tender?
+    quantity_surveyor_created_this_tenders_request_for_tender? &&
+        tender.reviewable?
   end
 
   def boq?
-    quantity_surveyor_created_this_tenders_request_for_tender?
+    quantity_surveyor_created_this_tenders_request_for_tender? &&
+        tender.reviewable?
   end
 
   def other_documents?
-    quantity_surveyor_created_this_tenders_request_for_tender?
+    quantity_surveyor_created_this_tenders_request_for_tender? &&
+        tender.reviewable?
   end
 
   def disqualify?
-    quantity_surveyor_created_this_tenders_request_for_tender?
+    quantity_surveyor_created_this_tenders_request_for_tender? &&
+        tender.reviewable?
   end
 
   def undo_disqualify?
-    quantity_surveyor_created_this_tenders_request_for_tender?
+    quantity_surveyor_created_this_tenders_request_for_tender? &&
+        tender.reviewable?
   end
 
   def rate?
-    quantity_surveyor_created_this_tenders_request_for_tender?
+    quantity_surveyor_created_this_tenders_request_for_tender? &&
+        tender.reviewable?
   end
 
   private
 
   def quantity_surveyor_created_this_tenders_request_for_tender?
-    # the request_for_tender that this tender belongs to, should have been
-    # created by the logged in quantity surveyor
-    quantity_surveyor.id  == tender.request_for_tender.quantity_surveyor.id &&
-        tender.reviewable?
+    quantity_surveyor.id  == tender.request_for_tender.quantity_surveyor.id
   end
 end
