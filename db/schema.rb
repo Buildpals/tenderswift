faf0edcc6c80c4faa0654b18975de54ce5e0b055
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-05-09 14:37:15 +0100

    Save QS's boq on cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503151139) do
+ActiveRecord::Schema.define(version: 20180509114241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,14 @@ ActiveRecord::Schema.define(version: 20180503151139) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_contractors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_contractors_on_reset_password_token", unique: true
+  end
+
+  create_table "excel_files", force: :cascade do |t|
+    t.string "document"
+    t.bigint "request_for_tender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["request_for_tender_id"], name: "index_excel_files_on_request_for_tender_id"
   end
 
   create_table "other_document_uploads", force: :cascade do |t|
