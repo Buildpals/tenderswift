b1807f8a8d9a55c53fd476da84ac854edec45591
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-05-27 22:55:21 +0000

    Code cleanup

--- app/javascript/uploadListOfItems/uploadListOfItems.vue
+++ app/javascript/uploadListOfItems/uploadListOfItems.vue
@@ -4,38 +4,19 @@
     <div class="d-flex justify-content-center mt-2 mb-3">
       <b-form-radio-group id="uploadSelector"
                           buttons
-                          button-variant="outline-primary"
+                          button-variant="outline-dark"
                           size="sm"
                           v-model="selected"
                           :options="options"
                           name="uploadSelector"/>
     </div>
 
-
     <create-list-of-items v-show="selected === '0'"
                           :request_for_tender_id="request_for_tender.id"
-                          :list_of_items="request_for_tender.list_of_items">
-
-      <slot name="previous-link">
-        <div slot="previous-link">
-          <%= link_to 'Previous', previous_wizard_path %>
-        </div>
-      </slot>
-
-    </create-list-of-items>
+                          :list_of_items="request_for_tender.list_of_items" />
 
     <excel-uploader v-show="selected === '1'"
-                    :request_for_tender="request_for_tender">
-
-      <div slot="previous-link">
-        <%= link_to 'Previous', previous_wizard_path %>
-      </div>
-
-      <div slot="next-link">
-        <%= link_to 'Save and continue', next_wizard_path %>
-      </div>
-
-    </excel-uploader>
+                    :request_for_tender="request_for_tender" />
   </div>
 </template>
 
