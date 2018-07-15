7b152811647584c0c9303c998a41a8fc0c745555
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-07-15 10:06:39 +0000

    Replace contenteditable divs with input[type=text] for editing cells

--- app/javascript/billOfQuantities/Cell.vue
+++ app/javascript/billOfQuantities/Cell.vue
@@ -1,10 +1,18 @@
 <template>
-  <td class="cell" v-bind:class="{ formula: cell && cell.f }">
-    <div :contenteditable="options && options.editableRates"
-         @click="showCellContents"
-         @blur="update">
-      {{ cell && cell.w }}
+  <td class="cell"
+      v-bind:class="{ formula: hasFormula }"
+      @click="showCellContents">
+
+    <input type="text"
+           class="form-control form-control-sm cell-input"
+           v-show="isEditable"
+           v-model="value"
+           @change="updateWorkbook">
+
+    <div v-show="!isEditable">
+      {{ value }}
     </div>
+
   </td>
 </template>
 
@@ -17,30 +25,57 @@
       'options'
     ],
 
+    data () {
+      return {
+        value: ''
+      }
+    },
+
+    mounted () {
+      if (this.cell && this.cell.v) {
+        this.value = this.cell.v
+      }
+    },
+
+    computed: {
+      isEditable () {
+        return this.cell && this.options && this.options.editableRates
+      },
+      hasFormula () {
+        return this.cell && this.cell.f
+      }
+    },
+
     methods: {
       showCellContents () {
         if (this.cell && this.cell.f) {
           this.$emit('show-cell-contents', this.cell.f)
-        } else if (this.cell && this.cell.w) {
-          this.$emit('show-cell-contents', this.cell.w)
+        } else if (this.cell && this.cell.v) {
+          this.$emit('show-cell-contents', this.cell.v)
         } else {
           this.$emit('show-cell-contents', '')
         }
       },
-      update (event) {        
-        let rate = event.target.innerText
-        EventBus.$emit('cell-change', { cellAddress: this.cellAddress, rate });
+      updateWorkbook (event) {
+        EventBus.$emit('cell-change', {
+          cellAddress: this.cellAddress,
+          value: event.target.value
+        })
       }
     }
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .formula {
     background: beige;
   }
 
   .cell {
     padding: 0rem !important;
+  }
+
+  .cell-input {
+    border-radius: 0;
   }
 </style>
