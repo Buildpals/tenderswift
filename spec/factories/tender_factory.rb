62dd36b77c977a0d2783649b59af3aae9f659d6e
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-07-21 08:29:23 +0000

    Set correct value for list_of_rates in tender_factory

--- spec/factories/tender_factory.rb
+++ spec/factories/tender_factory.rb
@@ -31,12 +31,12 @@ FactoryBot.define do
     end
 
     trait :filled do
-      list_of_rates('rates' => { '1' => '1.5',
-                                 '2' => '3',
-                                 '3' => '4',
-                                 '4' => '7',
-                                 '6' => '5' },
-                    'updated_at' => 1_527_587_444_641)
+      list_of_rates('Sheet1!E8' => 2, 'Sheet1!E11' => 1, 'Sheet1!E14' => 1,
+                    'Sheet1!E17' => 1, 'Sheet1!E20' => 1, 'Sheet1!E22' => 1,
+                    'Sheet1!E26' => 1, 'Sheet1!E35' => 1, 'Sheet1!E41' => 1,
+                    'Sheet1!E57' => 1, 'Sheet1!E66' => 1, 'Sheet1!E70' => 1,
+                    'Sheet1!E86' => 1)
+      sequence(:version_number) { |n| n }
 
       after(:create) do |tender|
         tender.request_for_tender.required_documents.each do |required_document|
@@ -48,12 +48,13 @@ FactoryBot.define do
     end
 
     trait :submitted do
-      list_of_rates('rates' => { '1' => '1.5',
-                                 '2' => '3',
-                                 '3' => '4',
-                                 '4' => '7',
-                                 '6' => '5' },
-                    'updated_at' => 1_527_587_444_641)
+      list_of_rates('Sheet1!E8' => 2, 'Sheet1!E11' => 1, 'Sheet1!E14' => 1,
+                    'Sheet1!E17' => 1, 'Sheet1!E20' => 1, 'Sheet1!E22' => 1,
+                    'Sheet1!E26' => 1, 'Sheet1!E35' => 1, 'Sheet1!E41' => 1,
+                    'Sheet1!E57' => 1, 'Sheet1!E66' => 1, 'Sheet1!E70' => 1,
+                    'Sheet1!E86' => 1)
+      sequence(:version_number) { |n| n }
+
 
       after(:create) do |tender|
         tender.request_for_tender.required_documents.each do |required_document|
