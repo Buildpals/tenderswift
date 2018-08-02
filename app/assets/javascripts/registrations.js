eb6b11fffd1aacb79dc743efc4215b62b527261f
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-08-02 11:32:09 +0000

    Improve UX of contractor after_signup screen - Add welcome message

--- app/assets/javascripts/registrations.js
+++ app/assets/javascripts/registrations.js
@@ -1,15 +1,17 @@
-$(document).on("turbolinks:load", function () {
-    if ($(".registrations.new, .publishers.edit, .contractors.edit").length === 0) return;
+$(document).on('turbolinks:load', function () {
+  let pagesToRunOn =
+    $('.registrations.new, .publishers.edit, .contractors.edit, .after_signup.edit')
+  if (pagesToRunOn.length === 0) return
 
-    let input = $("#phone_number_input");
-    input.intlTelInput({
-        initialCountry: "gh",
-        utilsScript: "/assets/libphonenumber/utils.js"
-    });
+  let input = $('#phone_number_input')
+  input.intlTelInput({
+    initialCountry: 'gh',
+    utilsScript: '/assets/libphonenumber/utils.js'
+  })
 
-    $("form").submit(function() {
-        let intlNumber = input.intlTelInput("getNumber");
-        $("#publisher_phone_number").val(intlNumber);
-        $("#contractor_phone_number").val(intlNumber);
-    });
-});
+  $('form').submit(function () {
+    let intlNumber = input.intlTelInput('getNumber')
+    $('#publisher_phone_number').val(intlNumber)
+    $('#contractor_phone_number').val(intlNumber)
+  })
+})
