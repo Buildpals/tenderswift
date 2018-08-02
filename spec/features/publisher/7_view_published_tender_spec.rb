1a94e969fc102938dde5d76b5bf6ba0852039e70
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-08-02 10:37:00 +0100

    Test display of 'ends on' date

--- spec/features/publisher/7_view_published_tender_spec.rb
+++ spec/features/publisher/7_view_published_tender_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.feature 'View details of published request for tender', js: true do
+  include RequestForTendersHelper
 	let!(:publisher) {FactoryBot.create(:publisher)}
   let!(:request_for_tender) do
 		FactoryBot.create(:request_for_tender,
@@ -18,11 +19,7 @@ RSpec.feature 'View details of published request for tender', js: true do
 		visit query_request_for_tender_path
 		fill_in 'reference_number', with: request_for_tender.id
 		click_button 'search'
-		visit new_publisher_session_path
-    fill_in 'Email address', with: publisher.email
-    fill_in 'Password', with: publisher.password
-		click_button 'Log in'
-		
+    sign_in_publisher
 		click_link request_for_tender.project_name
 
 		within :css, '#number-of-counts' do
@@ -32,11 +29,7 @@ RSpec.feature 'View details of published request for tender', js: true do
 	end
 
 	scenario 'should see view purchase counts' do
-		visit new_publisher_session_path
-    fill_in 'Email address', with: publisher.email
-    fill_in 'Password', with: publisher.password
-		click_button 'Log in'
-		
+    sign_in_publisher
 		click_link request_for_tender.project_name
 
 		within :css, '#number-of-purchases' do
@@ -46,15 +39,27 @@ RSpec.feature 'View details of published request for tender', js: true do
 	end
 
 	scenario 'should see number of submitted tenders' do
-		visit new_publisher_session_path
-    fill_in 'Email address', with: publisher.email
-    fill_in 'Password', with: publisher.password
-		click_button 'Log in'
-		
+		sign_in_publisher
 		click_link request_for_tender.project_name
 
 		within :css, '#number-of-submitted-tender' do
 			expect(page).to have_content request_for_tender.tenders.submitted.count
 		end
-	end
+  end
+
+  scenario 'should see ends on date' do
+    sign_in_publisher
+    click_link request_for_tender.project_name
+
+    within :css, '#deadline-request-for-tender' do
+      expect(page).to have_content time_to_deadline request_for_tender
+    end
+  end
+
+  def sign_in_publisher
+    visit new_publisher_session_path
+    fill_in 'Email address', with: publisher.email
+    fill_in 'Password', with: publisher.password
+    click_button 'Log in'
+  end
 end
