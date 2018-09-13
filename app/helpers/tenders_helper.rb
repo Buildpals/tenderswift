a59a4f773db3ecb75e35d48a4551f42991a5cd48
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-09-13 14:48:16 +0100

    Refactor hiding Boq tab for contractor

--- app/helpers/tenders_helper.rb
+++ app/helpers/tenders_helper.rb
@@ -10,4 +10,16 @@ module TendersHelper
     end
     tender
   end
+
+  def contractor_tab(tender, wizard_step, index)
+      if !tender.submitted_at.nil? && wizard_step.eql?(:upload_documents)
+         return link_to "#{index + 1}. Uploaded Documents",
+                wizard_path(wizard_step),
+                class: "nav-item nav-link #{'active' if step == wizard_step}"
+      else
+         return link_to "#{index + 1}. #{wizard_step.to_s.humanize}",
+                  wizard_path(wizard_step),
+                  class: "nav-item nav-link #{'active' if step == wizard_step}"
+      end
+  end
 end
