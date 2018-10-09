c35e1e0ab6dc45d04302fbd2846e08eaa8023228
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-10-09 16:26:49 +0100

    Sign Up with just email and company name

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -8,7 +8,8 @@ class Publisher < ApplicationRecord
          :recoverable,
          :rememberable,
          :trackable,
-         :validatable
+         :validatable,
+         :confirmable
 
   mount_uploader :company_logo, CompanyLogoUploader
 
@@ -20,16 +21,26 @@ class Publisher < ApplicationRecord
 
   EMAIL_MESSAGE = 'Please provide an correct email address with more than 5 characters'
 
-  validates :company_name, presence: true
+  #validates :company_name, presence: true
 
   validates :email, presence: { message: EMAIL_MESSAGE },
                     uniqueness: { message: EMAIL_MESSAGE,
                                   case_sensitive: false },
                     format: EMAIL_REGEX
 
-  validates :phone_number, presence: true
+  #validates :phone_number, presence: true
 
   def name
     "#{company_name} (#{email})"
+  end
+
+  protected
+
+  def password_required?
+    confirmed? ? super : false
+  end
+
+  def confirmation_required?
+    false
   end
 end
