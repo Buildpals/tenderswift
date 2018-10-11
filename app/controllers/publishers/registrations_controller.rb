de22979b92010292a77c0577e42edf147660a5fa
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-10-11 18:25:53 +0000

    Allow signing up from external form
    This will allow post requests without an authenticity_token, say from an
    external page or application, to pass through.
    
    The goal here is to allow signup forms on our landing page to work

--- app/controllers/publishers/registrations_controller.rb
+++ app/controllers/publishers/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Publishers::RegistrationsController < Devise::RegistrationsController
   #include Accessible
+  skip_before_action :verify_authenticity_token, only: :create
 
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
