2277dd3408dca11dbffff765d61677328d4e2a00
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-11-14 09:48:48 +0000

    Fix more errors

--- app/assets/javascripts/publisher.js
+++ app/assets/javascripts/publisher.js
@@ -1,8 +1,11 @@
 $(document).on("turbolinks:load", function () {
-  if ($(".build.show").length === 0) return;
-  $("#surveyModal").modal("show");
+  if ($(".build.show").length === 0){
+    return;
+  } else{
+    $("#surveyModal").modal("show");
+  }
 
   $(".vote").click( function (e) {
     $("#surveyModal").modal("hide");
   });
-})
+});
