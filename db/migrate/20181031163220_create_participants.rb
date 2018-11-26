3544e33f9ac63db7a5231da53bb2214b7fbe4b74
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-11-26 15:18:13 +0000

    Drop participants table first to fix heroku error

--- db/migrate/20181031163220_create_participants.rb
+++ db/migrate/20181031163220_create_participants.rb
@@ -1,5 +1,6 @@
 class CreateParticipants < ActiveRecord::Migration[5.1]
   def change
+    drop_table :participants
     create_table :participants do |t|
       t.references :request_for_tender
       t.string :email
