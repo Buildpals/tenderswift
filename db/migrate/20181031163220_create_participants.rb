8233ac638b4f62f7b7864cfa353626e0008b7d46
Kwaku Boateng <kwaku.ahonya@gmail.com>
2018-12-13 14:35:49 +0000

    Comment out creation of participant

--- db/migrate/20181031163220_create_participants.rb
+++ db/migrate/20181031163220_create_participants.rb
@@ -1,11 +1,11 @@
 class CreateParticipants < ActiveRecord::Migration[5.1]
   def change
-    drop_table :participants
-    create_table :participants do |t|
-      t.references :request_for_tender
-      t.string :email
+    #drop_table :participants
+    #create_table :participants do |t|
+    #  t.references :request_for_tender
+    #  t.string :email
 
-      t.timestamps
-    end
+    #  t.timestamps
+    #end
   end
 end
