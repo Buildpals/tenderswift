d43038ff917a3e80542c781354f1620aeabe8b26
Peter O'Hara Adu <ohara.invent@gmail.com>
2018-12-19 13:01:46 +0000

    Stop sending slack notifications in non-production environments

--- app/controllers/purchase_tender_controller.rb
+++ app/controllers/purchase_tender_controller.rb
@@ -157,7 +157,7 @@ class PurchaseTenderController < ContractorsController
   end
 
   def send_slack_notification(channel, message)
-    if Rails.env.production? || true
+    if Rails.env.production?
       webhook_url = 'https://hooks.slack.com/services/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'
       icon_url = 'https://res.cloudinary.com/tenderswift/image/upload/v1520934320/tenderswift-logo-square.png'
       notifier = Slack::Notifier.new(webhook_url) do
